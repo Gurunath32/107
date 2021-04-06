17443bd902c10f90d1ff1933f543c408dcf9300e
Gurunath32 <73350586+Gurunath32@users.noreply.github.com>
2021-04-06 22:41:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+prediction1="";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach(camera);
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'">';
+    })
+}
+
+console.log("ml5 Version:",ml5.version);
+
+classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/hqbIzbjUX/",modalloaded);
+
+function modalloaded() {
+    console.log("Modal Loaded!");
+}
